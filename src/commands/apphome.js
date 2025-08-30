4b2ad7f393a63b5cc3c3261d15b7c6943927f9dc
BananaJeans <18456310+BananaJeanss@users.noreply.github.com>
2025-08-30 16:24:54 +0300

    feat: add basic commands table to app home

--- src/commands/apphome.js
+++ src/commands/apphome.js
@@ -39,13 +39,12 @@ async function publishHome(client, userId) {
   midnight.setHours(0, 0, 0, 0);
   const todayStart = midnight.getTime();
 
-  const usageCounts =
-    (await getRow(
-      `SELECT
+  const usageCounts = (await getRow(
+    `SELECT
         (SELECT COUNT(*) FROM roast_usage WHERE slack_user_id = ? AND timestamp > ?) AS hourly_usage,
         (SELECT COUNT(*) FROM roast_usage WHERE slack_user_id = ? AND timestamp > ?) AS daily_usage`,
-      [userId, oneHourAgo, userId, todayStart]
-    ).catch(() => null)) || { hourly_usage: 0, daily_usage: 0 };
+    [userId, oneHourAgo, userId, todayStart]
+  ).catch(() => null)) || { hourly_usage: 0, daily_usage: 0 };
 
   const hourlyRoastUsage = usageCounts.hourly_usage || 0;
   const dailyRoastUsage = usageCounts.daily_usage || 0;
@@ -54,6 +53,7 @@ async function publishHome(client, userId) {
 
   const blocks = [
     {
+      // banner
       type: 'image',
       image_url:
         'https://github.com/BananaJeanss/slack.fm/raw/main/assets/slackfmbanner.png',
@@ -63,7 +63,7 @@ async function publishHome(client, userId) {
       type: 'header',
       text: { type: 'plain_text', text: 'slack.fm', emoji: true },
     },
-    { type: 'divider' },
+    // basic user bot stats
     username
       ? {
           type: 'section',
@@ -79,20 +79,219 @@ async function publishHome(client, userId) {
             text: '🔗 Not linked yet. Use `/link` to connect your Last.fm account.',
           },
         },
-    {
-      type: 'section',
-      text: { type: 'plain_text', text: usageLine, emoji: true },
-    },
+    ...(username
+      ? [
+          {
+            type: 'section',
+            text: { type: 'plain_text', text: usageLine, emoji: true },
+          },
+        ]
+      : []),
     { type: 'divider' },
     {
+      // commands
       type: 'header',
-      text: { type: 'plain_text', text: 'Commands', emoji: true },
+      text: {
+        type: 'plain_text',
+        text: 'Basic Commands',
+      },
+    },
+    {
+      type: 'table',
+      rows: [
+        // Header row
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  { type: 'text', text: 'Command', style: { bold: true } },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  { type: 'text', text: 'Description', style: { bold: true } },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  { type: 'text', text: 'Usage', style: { bold: true } },
+                ],
+              },
+            ],
+          },
+        ],
+        // /link
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: '/link' }],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: 'Link your Last.fm account' }],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: '/link' }],
+              },
+            ],
+          },
+        ],
+        // /unlink
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: '/unlink' }],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: 'Unlink your account' }],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: '/unlink' }],
+              },
+            ],
+          },
+        ],
+        // /profile
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: '/profile' }],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: 'Show profile stats' }],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: '/profile [@user]' }],
+              },
+            ],
+          },
+        ],
+        // /nowplaying
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: '/nowplaying' }],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: 'Current or last track' }],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: '/nowplaying [@user]' }],
+              },
+            ],
+          },
+        ],
+        // /recent
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: '/recent' }],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: 'Last 5 tracks' }],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [{ type: 'text', text: '/recent [@user]' }],
+              },
+            ],
+          },
+        ],
+      ],
     },
     {
       type: 'section',
       text: {
         type: 'mrkdwn',
-        text: 'The commands list is viewable via /slackfmcommands or <https://github.com/BananaJeanss/slack.fm/blob/main/commands.md>',
+        text: 'The full commands list is viewable via <https://github.com/BananaJeanss/slack.fm/blob/main/commands.md>',
       },
     },
   ];
